41fbf5f43483e6748669009b7332669886b835fe
Ben Cawkwell <bencawkwell@gmail.com>
2010-10-19 17:00:38 +0200

    Now selects first discussion in the list by default.

--- root/static/js/jquery.commentSelection.js
+++ root/static/js/jquery.commentSelection.js
@@ -366,6 +366,9 @@ function displayDiscussions(comments) {
             }
         }
     );
+    if(comments.length) {
+        template.siblings('.discussion:first').click();
+    }
 }
 function clearComments() {
     // Remove any previously selected text (blue) and highlight the relevent text
